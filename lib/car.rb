4b8fe7a414e00a996011c9b7aec676426c6d16f8
J K <kj688895@gmail.com>
2020-09-27 20:47:31 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -2,8 +2,8 @@ require_relative "./vehicle.rb"
 #requiring lib/vehicle.rb bc Car class needs access to Vehicle class
 class Car < Vehicle
 # symbol < is to inherit
-def go
-  "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!"
-end 
+  def go
+    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!"
+  end 
 
 end
